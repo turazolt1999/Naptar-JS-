652f2d8f7a95ef8b1e3607825326f0c5c5bc0b21
turazolt1999 <turan.zoltan0614@gmail.com>
2022-11-02 01:48:34 +0100

    finaly

--- calendar.js
+++ calendar.js
@@ -1,13 +1,5 @@
-
-/**
- * Véletlenszerűen generált festményt készít a paraméterül kapott pozícióra.
- * @param x Festmény bal felső sarkának x koordinátja.
- * @param y Festmény bal felső sarkának y koordinátja.
- * @param width Festmény szélessége.
- * @param height Festmény magassága.
- * @param color A festmény színe.
- */
-function generateImage(x, y, width, height, color) {
+let ido=5;
+function naptarImage(x, y, width, height, color, nap) {
     ctx.beginPath();
     ctx.strokeStyle = "#000000";
     ctx.fillStyle = "#ffffff";
@@ -16,47 +8,87 @@ function generateImage(x, y, width, height, color) {
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.moveTo(x, y + Math.random() * height);
-    ctx.lineTo(x + width, y + Math.random() * height);
+    ctx.fontStyle = "13px Ariel";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("   Hé   Ke  SZ  CS   Pé   SZ   Va",x+20,y+20);
+    var xa=x+30;
+    var ya=y+40;
+    var szamlalo=0;
+    var seged=ido;
+    for (let i=1; i<ido+1; i++) {
+        xa=xa+20;
+        seged=seged-1;
+        szamlalo++;
+    }
+    if (seged===7) {
 
-    ctx.moveTo(x, y + Math.random() * height);
-    ctx.lineTo(x + width, y + Math.random() * height);
-    ctx.stroke();
+    }
+    for (var i=1; i<nap+1; i++) {
+        if (szamlalo>6) {
+            szamlalo=0;
+            ya=ya+30;
+            xa=x+30;
+        }
 
-    var minWh = width < height ? width : height;
-    ctx.beginPath();
-    ctx.arc(x + width / 4 + Math.random() * minWh/2,
-        y +  height / 4 + Math.random() * height/2,
-        minWh/4,
-        0,
-        2 * Math.PI
-    );
+        ctx.fillText(i, xa, ya);
+        xa=xa+20;
+        szamlalo++;
+    }
+    ido=szamlalo;
     ctx.stroke();
 }
 
 // Festmények adatai
-var pictures = [
+var honapok = [
     {
-        favouriteColor: "#62ff9b",
-        name: "Picasso"
+        name: "Január",
+        nap: 31
     },
     {
-        favouriteColor: "#598dff",
-        name: "Da Vinci"
+        name: "Február",
+        nap:28
     },
     {
-        favouriteColor: "#ff9456",
-        name: "Van Gogh"
+        name: "Március",
+        nap: 31
     },
     {
-        favouriteColor: "#a8f5ff",
-        name: "Monet"
+        name: "Április",
+        nap: 30
     },
     {
-        favouriteColor: "#ff6f8c",
-        name: "Dali"
-    }
+        name: "Május",
+        nap: 31
+    },
+    {
+        name: "Június",
+        nap: 30
+    },
+    {
+        name: "Július",
+        nap: 31
+    },
+    {
+        name: "Augusztus",
+        nap: 31
+    },
+
+    {
+        name: "Szeptember",
+        nap: 30
+    },
+    {
+        name: "Oktober",
+        nap: 31
+    },
+    {
+        name: "November",
+        nap: 30
+    },
+    {
+        name: "December",
+        nap: 31
+    },
 ];
 
 var c = document.getElementById("ex");
@@ -64,43 +96,30 @@ var ctx = c.getContext("2d");
 
 // Háttér
 ctx.fillStyle = "#DDE0EB";
-ctx.fillRect(0, 0, 800, 600);
+ctx.fillRect(0, 0, 800, 800);
 
-// Falfestés (kerítés csíkozás)
-ctx.fillStyle = "#759ECB";
-ctx.fillRect(0, 300, 800, 10);
 
-var start = 20;
-while (start < 800) {
-    ctx.fillRect(start, 300, 16, 200);
-    start += 60;
-}
-
-// Padló
-ctx.fillStyle = "#E9DDDD";
-ctx.beginPath();
-ctx.rect(0, 500, 800, 100);
-ctx.stroke();
-ctx.fill();
-
-// Festmények kirajzolása
-var picPosX = 40;
-var picPisY = 60;
-var height = 160;
-var width = 100;
-var space = 60;
-for (var i in pictures) {
-    var pic = pictures[i];
-    generateImage(picPosX, picPisY, width, height, pic.favouriteColor);
-
+// Naptár kirajzolása
+var picPosX = 0;
+var picPisY = 0;
+var height = 200;
+var width = 200;
+for (var i in honapok) {
+    var honap = honapok[i];
+    naptarImage(picPosX, picPisY, width, height, honap.favouriteColor, honap.nap);
     ctx.beginPath();
     ctx.fillStyle = "#ffffee";
-    ctx.rect(picPosX + 20, picPisY + height + 20, 60, 20)
+    ctx.rect(picPosX + 70, picPisY + height + 20, 70, 20)
     ctx.fill();
-    ctx.fontStyle = "12px Ariel";
+    ctx.fontStyle = "13px Ariel";
     ctx.fillStyle = "#000000";
-    ctx.fillText(pic.name, picPosX + 24, picPisY + height + 34)
+    ctx.fillText(honap.name, picPosX + 83, picPisY + height + 34);
 
-    picPosX += width + space;
+    picPosX += width;
+
+    if (picPosX>800) {
+        picPisY += height + 50;
+        picPosX = 0;
+    }
+    ctx.stroke();
 }
-
